08c6a6e055b0d7868fb40bfa28538123b38cf0a9
TarkanKara <unnahmet23@hotmail.com>
2022-10-20 13:30:31 +0300

    riverpod1 || riverpod kullanimi

--- README.md
+++ README.md
@@ -1,4 +1,34 @@
 # provider_riverpod_kullanimi
 
-## provider1 Branch
-## provider2 Branch
+## riverpod: ^2.0.2
+## flutter_riverpod: ^2.0.2
+
+>>
+Provider paket kullanımında ağacımıza veriyi enjekte edeceğimiz zamanda ağacın en yukarılarında bir yerlere veriyorduk ki
+uyguylamanın altında hiyeraşik olarak buna ihtiyaç duyan alt basamaktaki diğer widgetler yukarıya baktıklarında veriye istediği gibi
+ulaşabilsin burada aynı tipte başka veri karşılarına çıktığında malasef yanlış veriyide bizlere getirebilir.Bu istemediğimiz bir durum
+>>
+Projemizde riverpod u kullanabilmek için ilk widgetimiz olan MyApp widgetini bir widget olan ProviderScope widgeti ile sarmalıyoruz.
+ProviderScope (flutter_riverpod: ^2.0.2 paketinden geliyor.) Bu yüzden projemizde flutter_riverpod paketini kullanmalıyız.
+>>
+Widgetimizi ProviderScopeile sarmaladıktan sonra Provider yardımıyla bir değişken oluşturalım.
+riverpod paketi ile nesnelerimizi provider edeceğimiz zaman 
+kullanım şu şekildedir. 
+>>
+void main() => runApp(const ProviderScope(child: MyApp()));
+>>
+Hello world verisini ilgili widgette kullanmaya başlayalım
+>>
+verimizi ilgili widgette kullanabilmek için Consumr widgetten extends etmemiz gerekiyor.
+>>
+Consumer widgetin build fonksiyonu (BuildContext context ve WidgetRef ref) adnda iki paramaetre alır.
+>>
+Consumer widgetin build fonksiyonu;
+>>
+Widget build(BuildContext context, WidgetRef ref)
+>>
+Consumer widgetini düzenlendikten sonra Data yı text widgetinde kullanalım. bize verilen ref değişkeni ile read parametresinden sonra datanın tipini belirleyerek provider edilecek nesnenin adını veriyoruz.
+>>
+ref.read<String>(helloWorld), kullanım bu şekilde
+>>
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() => runApp(const MyApp());
+//ProviderScope MyApp widgeti ile sarmalıyoruz.
+void main() => runApp(const ProviderScope(child: MyApp()));
+
+//Riverpod ile nesnelerimizi provider etmek
+//Hello world verisini ilgili widgette kullanmaya başlayalım
+final helloWorld = Provider((ref) => "Hello World");
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
+      home: HomePage(),
+    );
+  }
+}
+
+//verimizi burda kullanabilmek için Consumr widgtetten extends etmemiz gerekiyor.
+//Consumer widgetin build fonksiyonu (BuildContext context ve WidgetRef ref) adnda iki paramaetre alır.
+
+class HomePage extends ConsumerWidget {
+  const HomePage({super.key});
+
+  @override
+  //Consumer widgetin build fonksiyonu
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Riverpod'),
+        centerTitle: true,
+      ),
+      //Consumer widgetini düzenlendikten sonra
+      //Data yı text widgetinde kullanalım
+      //bize verilen ref değişkeni ile read parametresi dedikten sonra datanın tipini belirleyerek
+      //provider edilecek nesnenin adını veriyoruz.
+      //ref.read<String>(helloWorld), kullanım bu şekilde
+      body: Center(
+        child: Text(
+          ref.read<String>(helloWorld),
+          style: const TextStyle(fontSize: 55),
         ),
       ),
     );
